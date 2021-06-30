3d9727868a9207db7e35f4abf7cf1d9eefa678a9
Joe Sobo <joe@metalpay.co>
2021-06-29 17:26:57 -0700

    updated styling for weather module

--- components/WeatherModule.tsx
+++ components/WeatherModule.tsx
@@ -1,10 +1,13 @@
 import React from "react";
-import { Dimensions, StyleSheet, View } from "react-native";
+import { Dimensions, StyleSheet, View, Text } from "react-native";
 import { ProgressChart } from "react-native-chart-kit";
+import { LinearGradient } from "expo-linear-gradient";
 
 const width = Dimensions.get("window").width; //full width
 
 const WeatherModule = () => {
+  const days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+
   const data1 = {
     data: [0.75],
   };
@@ -12,15 +15,25 @@ const WeatherModule = () => {
   const chartConfig = {
     backgroundGradientFrom: "#fff",
     backgroundGradientTo: "#fff",
-    color: (opacity = 1) => `rgba(144, 195, 91, ${opacity})`,
+    color: (opacity = 1) => `rgba(154, 192, 98, ${opacity})`,
     strokeWidth: 2, // optional, default 3
-    barPercentage: 0.5,
+    barPercentage: 0.75,
     useShadowColorFromDataset: false, // optional
     fillShadowGradient: "#000",
     fillShadowGradientOpacity: 1,
   };
 
-  const { row, barChart, bar, barBackground } = styles;
+  const {
+    row,
+    barChart,
+    bar,
+    barBackground,
+    temperature,
+    tempValue,
+    barColumn,
+    dayText,
+    gradient,
+  } = styles;
 
   return (
     <View style={row}>
@@ -34,27 +47,26 @@ const WeatherModule = () => {
         hideLegend={true}
       />
       <View style={barChart}>
-        <View style={barBackground}>
-          <View style={bar} />
-        </View>
-        <View style={barBackground}>
-          <View style={bar} />
-        </View>
-        <View style={barBackground}>
-          <View style={bar} />
-        </View>
-        <View style={barBackground}>
-          <View style={bar} />
-        </View>
-        <View style={barBackground}>
-          <View style={bar} />
-        </View>
-        <View style={barBackground}>
-          <View style={bar} />
-        </View>
-        <View style={barBackground}>
-          <View style={bar} />
-        </View>
+        {days.map((day) => {
+          return(
+            <View key={day} style={barColumn}>
+              <View style={barBackground}>
+                <View style={bar}>
+                  <LinearGradient
+                    colors={["#D3ECA3bc", "#9DC569bc"]}
+                    start={[1, 0]}
+                    end={[0, 1]}
+                    style={gradient}
+                  ></LinearGradient>
+                </View>
+              </View>
+              <Text style={dayText}>{day}</Text>
+            </View>
+          )
+        })}
+      </View>
+      <View style={temperature}>
+        <Text style={tempValue}>90 °F</Text>
       </View>
     </View>
   );
@@ -66,6 +78,7 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     paddingLeft: 4,
     paddingRight: 12,
+    position: "relative",
   },
   barChart: {
     width: width - 24 - 125 - 18,
@@ -73,23 +86,51 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     alignItems: "flex-end",
     justifyContent: "space-between",
+    marginBottom: 8,
   },
   bar: {
     width: 12,
     height: 60,
-    backgroundColor: "#9ac5659a",
-    borderTopLeftRadius: 12,
-    borderTopRightRadius: 12,
+    borderRadius: 12,
+    overflow: "hidden",
+  },
+  gradient: {
+    flex: 1,
   },
   barBackground: {
     width: 12,
     height: 80,
-    backgroundColor: "#e7e7e7",
-    borderTopLeftRadius: 12,
-    borderTopRightRadius: 12,
+    backgroundColor: "#F1F3EF",
+    borderRadius: 12,
     display: "flex",
     flexDirection: "row",
     alignItems: "flex-end",
+  },
+  temperature: {
+    height: 125,
+    width: 125,
+    position: "absolute",
+    left: 0,
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    marginLeft: 5,
+  },
+  tempValue: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "#656965",
+  },
+  barColumn: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  dayText: {
+    fontSize: 12,
+    fontWeight: "bold",
+    color: "#CBCBCB",
   },
 });
 
